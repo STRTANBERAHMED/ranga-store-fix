895aa719bf61ca5806ca40cb379d8dc0194d98ae
strtanberahmed <tanber1111@gmail.com>
2021-09-13 14:58:28 +0300

    final commit

--- js/app.js
+++ js/app.js
@@ -21,8 +21,8 @@ const showProducts = (products) => {
       <p>Average Rating: ${product.rating.rate}</p>
       <p>Total Rating Count: ${product.rating.count}</p>
       <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-white">add to cart</button>
+      <button id="details-btn" class="btn details-btn">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
